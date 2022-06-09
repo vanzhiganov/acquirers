5ccdb62cc9953b9512c51c7c3038bc508fa98248
Vyacheslav Anzhiganov <vanzhiganov@ya.ru>
2022-06-09 10:49:03 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='Acquirers',
     version='0.0.5',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vanzhiganov/acquirers/",
+    project_urls={
+        "Bug Tracker": "https://github.com/vanzhiganov/acquirers/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 license",
+        "Operating System :: OS Independent",
+    ],
     packages=[
         'Acquirers',
         'Acquirers.Tinkoff',
